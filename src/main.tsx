43e50f38acad2de579548866ba4562711204e3a0
Chandan Kumar <kumarnchandan05@gmail.com>
2025-01-31 02:45:01 +0530

    error fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 import { All_Projects, All_Experience, ErrorPage } from './pages';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route >,
-      <Route path="/" element={<App />} errorElement={<ErrorPage />} />
-      <Route path='/projects' element={<All_Projects />} errorElement={<ErrorPage />} />
-      <Route path='/experience' element={<All_Experience />} errorElement={<ErrorPage />} />
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'projects',
+        element: <All_Projects />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: 'experience',
+        element: <All_Experience />,
+        errorElement: <ErrorPage />,
+      }
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <RouterProvider router={router} />
+    </BrowserRouter>
   </StrictMode>,
 )
