25b6d1990da15925c737632d4571379388e3054a
Chandan Kumar <kumarnchandan05@gmail.com>
2025-01-31 02:06:12 +0530

    error fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 import { All_Projects, All_Experience, ErrorPage } from './pages';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/projects',
-    element: <All_Projects />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/experience',
-    element: <All_Experience />,
-    errorElement: <ErrorPage />,
-  }
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<ErrorPage />}>,
+      <Route path='projects' element={<All_Projects />} errorElement={<ErrorPage />} />
+      <Route path='experience' element={<All_Experience />} errorElement={<ErrorPage />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <RouterProvider router={router} />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
