2d6252b173e6a32d2c3ab5523b49840352ce03f7
Chandan Kumar <kumarnchandan05@gmail.com>
2025-01-31 02:30:26 +0530

    error fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,8 @@ import { All_Projects, All_Experience, ErrorPage } from './pages';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/home" element={<App />} errorElement={<ErrorPage />}>,
+    <Route >,
+      <Route path="/home" element={<App />} errorElement={<ErrorPage />} />
       <Route path='/home/projects' element={<All_Projects />} errorElement={<ErrorPage />} />
       <Route path='/home/experience' element={<All_Experience />} errorElement={<ErrorPage />} />
     </Route>
