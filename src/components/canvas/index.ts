c6c30a0cfb4287994d48c37197e06075629edb83
Chandan Kumar <kumarnchandan05@gmail.com>
2025-02-28 16:52:09 +0530

    code snippet hero section

--- src/components/canvas/index.ts
+++ src/components/canvas/index.ts
@@ -1,6 +1,5 @@
 import BallCanvas from './Ball';
 import EarthCanvas from './Earth';
 import StarsCanvas from './Stars';
-import ComputerCanvas from './Computers';
 
-export { BallCanvas, EarthCanvas, StarsCanvas, ComputerCanvas };
+export { BallCanvas, EarthCanvas, StarsCanvas};
